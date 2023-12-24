d1175db5db45332f416dccc1c30c183f2dc651db
Poorneshcoder <eswar.chinni354@gmail.com>
2023-12-24 13:21:08 +0530

    action_basics

--- src/Pages/Orders.js
+++ src/Pages/Orders.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../Redux/ReduxAction/actionCreators';
 
 const Orders = () => {
+  // variables for the function......
     const reduxState = useSelector((state)=>state);
+    const dispatch = useDispatch();
+    // const purchaseDispatch = useDispatch(purchaseProduct)
+    // const restoreDispatch = useDispatch(restoreProduct)
+
+    // const allActions = bindActionCreators(actionCreators, dispatch);
+    // console.log(allActions);
+
+    const {purchaseProduct, restoreProduct} = bindActionCreators(actionCreators,dispatch);
+
     console.log(reduxState);
+
+    const [restore, setRestore] = useState();
+    const [purchase, setPurchase] = useState();
   return (
     <div>
       <h1>Number of Orders</h1>
+      overall inventory : {reduxState.inventory} {' '}
+      <div>
+        <input
+        value={restore}
+        placeholder='restore'
+        onChange={(e)=>setRestore(e.target.value)}
+        />
+
+        <input
+        value={purchase}
+        placeholder='purchase'
+        onChange={(e)=>setPurchase(e.target.value)}
+        />
+      </div>
+      <button
+      onClick={()=>restoreProduct(restore)}
+      >add inventory</button>
+      {" "}
+      <button
+      onClick={()=>purchaseProduct(purchase)}
+      >purchase order</button>
     </div>
   )
 }
--- src/Redux/ReduxReducer/inventoryReducer.js
+++ src/Redux/ReduxReducer/inventoryReducer.js
@@ -1,10 +1,10 @@
 const inventoryReducers = (state = 0, action) => {
     switch (action.type){
         case "restore_the_inventory":
-            return state + action.payload;
+            return state + +action.payload;
 
         case "purchase_list":
-            return state - action.payload;
+            return state - +action.payload;
 
         default:
             return state;
